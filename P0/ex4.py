e9aff50d931381199c36f1c33d6d6a9fec913a0a
alvarogm <a.gonzalezm.2021@alumnos.urjc>
2022-03-16 17:44:25 +0100

    Practice 0

--- P0/ex4.py
+++ P0/ex4.py
@@ -1,4 +1,10 @@
 import seq0
 exit = False
 while not exit:
-    filename, gene = seq0.get_file()
+    filename = seq0.valid_filename()
+    if filename == "none":
+        exit = True
+    else:
+        seq = seq0.seq_read_fasta(filename)
+        bases_a, bases_c, bases_g, bases_t = seq0.seq_bases(seq)
+        print(count_a, count_c, count_g, count_t)
