c1238939592fcef07996287466632c1acad81b9a
Alvarogonme <a.gonzalezm.2021@alumnos.urjc.es>
2022-03-21 15:35:18 +0100

    Practice 3

---
+++ P3/server.py
@@ -0,0 +1,124 @@
+import socket
+from colorama import init, Fore
+from seq1 import Seq
+
+# -- Step 1: create the socket
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# -- Optional: This is for avoiding the problem of Port already in use
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# Configure the Server's IP and PORT
+PORT = 6123
+IP = "127.0.0.1"
+seq0 = "AGAGAGAGAGAGAGA"
+seq1 = "ATATATATATATATA"
+seq2 = "CACACACACACACAC"
+seq3 = "TGTGTGTGTGTGTGT"
+seq4 = "CGCGCGCGCGCGCGC"
+
+sequences = [seq0, seq1, seq2, seq3, seq4]
+
+# -- Step 1: create the socket
+ls = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# -- Optional: This is for avoiding the problem of Port already in use
+ls.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+
+# -- Step 2: Bind the socket to server's IP and PORT
+ls.bind((IP, PORT))
+
+# -- Step 3: Configure the socket for listening
+ls.listen()
+
+print("The server is configured!")
+
+while True:
+    # -- Waits for a client to connect
+    print("Waiting for Clients to connect")
+
+    try:
+        (cs, client_ip_port) = ls.accept()
+
+        # -- Server stopped manually
+    except KeyboardInterrupt:
+        print("Server stopped by the user")
+
+        # -- Close the listenning socket
+        ls.close()
+
+        # -- Exit!
+        exit()
+    else:
+        init(autoreset=True)
+
+        # -- Read the message from the client
+        # -- The received message is in raw bytes
+        msg_raw = cs.recv(2048)
+
+        # -- We decode it for converting it
+        # -- into a human-redeable string
+
+        msg = msg_raw.decode().replace("\n", "").strip()
+        splitted_command = msg.split(" ")
+        cmd = splitted_command[0]
+
+
+
+        # -- Print the received message
+
+        if cmd != "PING":
+            arg = splitted_command[1]
+            print(Fore.LIGHTGREEN_EX + cmd)
+
+        if cmd == "PING":
+            response = "OK\n"
+            cs.send(response.encode())
+        elif cmd == "GET":
+                response = sequences[int(arg)]
+                cs.send(response.encode())
+                print(sequences[int(arg)])
+
+        elif cmd == "INFO":
+            print("Sequence:", arg)
+            print("The length:", len(arg))
+            s = Seq(arg)
+            d = s.count()
+            for k, v in d.items():
+                print(k + ":", str(v), str(round((v / len(arg)) * 100, 2)) + "%\n", end=" ")
+            response = f"The sequence: {arg}"
+            cs.send(response.encode())
+            response = f"\nThe lenght of the sequence: {str(len(arg))}"
+            # -- The message has to be encoded into bytes
+            cs.send(response.encode())
+            for k, v in d.items():
+                    response = f"\n{k} : {str(v)} ({round((v / len(arg)) * 100, 2)})%"
+                    cs.send(response.encode())
+        elif cmd == "COMP":
+            s = Seq(arg)
+            complementary = s.complement()
+            print(complementary)
+            response4 = complementary
+            cs.send(response4.encode())
+        elif cmd == "REV":
+            s = Seq(arg)
+            reverse = s.reverse()
+            print(reverse)
+            response5 = reverse
+            cs.send(response5.encode())
+        elif cmd == "GENE":
+            s = Seq()
+            s.read_fasta2(arg)
+            print(s)
+            response = str(s)
+            cs.send(response.encode())
+
+
+        else:
+            # -- Send a response message to the client
+            response = "This comand is not available in the server\n"
+
+
+
+        # -- Close the data socket
+        cs.close()
---
+++ P3/test-client.py
@@ -0,0 +1,56 @@
+from client0 import Client
+from seq1 import Seq
+
+
+PRACTICE = 3
+EXERCISE = 7
+
+print(f"-----| Practice {PRACTICE}, Exercise {EXERCISE} |------")
+
+# -- Parameters of the server to talk to
+IP = "127.0.0.1"
+PORT = 6123
+# -- Print the IP and PORTs
+c = Client(IP, PORT)
+print(c)
+exit = False
+while not exit:
+    option = input("What command do you want to test? ")
+    if option.lower() == "ping":
+        print("*Testing PING...")
+        response = c.talk("PING")
+        print(response)
+    elif option.lower() == "get":
+        print("*Testing GET...")
+        exit = False
+        while not exit:
+            option2 = input("Which sequence do you want to get? (or exit)")
+            if option2.lower() == "exit":
+                exit = True
+            else:
+                response = c.talk(f"GET {option2}")
+                print(response)
+    elif option.startswith("INFO"):
+        print("Testing INFO command...")
+        response = c.talk(option)
+        print(response)
+    elif option.startswith("COMP"):
+        print("Testing que COMP command...")
+        response = c.talk(option)
+        print(response)
+    elif option.startswith("REV"):
+        print("Testing the REV command...")
+        response = c.talk(option)
+        print(response)
+    elif option.lower() == "gene":
+        print("*Testing GENE...")
+        exit = False
+        while not exit:
+            option2 = input("Which gene do you want to get? (or exit)")
+            if option2.lower() == "exit":
+                exit = True
+            else:
+                response = c.talk(f"GENE {option2}")
+                print(response)
+    elif option.lower == "exit":
+        exit = True
